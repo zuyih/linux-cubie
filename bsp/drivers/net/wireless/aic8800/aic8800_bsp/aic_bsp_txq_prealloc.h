209b84eec43eaa647abdef96bb7ec14dbdd70236
Feng Zhang <feng@radxa.com>
2025-06-23 17:24:54 +0800

    bsp: drivers/net: aic8800: remove allwinner aic8800 driver
    Signed-off-by: Feng Zhang <feng@radxa.com>

--- bsp/drivers/net/wireless/aic8800/aic8800_bsp/aic_bsp_txq_prealloc.h
+++
@@ -1,7 +0,0 @@
-#ifndef _AIC_BSP_TXQ_PREALLOC_H
-#define _AIC_BSP_TXQ_PREALLOC_H
-
-void aicwf_prealloc_txq_free(void);
-void *aicwf_prealloc_txq_alloc(size_t size);
-
-#endif
--- bsp/drivers/net/wireless/aic8800/aic8800_fdrv/aicwf_8800d80.h
+++
@@ -1,9 +0,0 @@
-#ifndef _AICWF_COMPAT_8800D80_H_
-#define _AICWF_COMPAT_8800D80_H_
-#include <linux/types.h>
-#include "aic_bsp_export.h"
-
-int aicwf_set_rf_config_8800d80(struct rwnx_hw *rwnx_hw, struct mm_set_rf_calib_cfm *cfm);
-
-#endif
-
